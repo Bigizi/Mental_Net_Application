153c6a6b5d3a8b5b3f7d94aced27a18cddc98025
Crispin <bigizinduwayo@gmail.com>
2022-07-21 21:33:06 +0200

    First commit

---
+++ src/Components/LandPage/Home.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './home.css';
+
+export default function Home() {
+  return (
+    <div className='container'>
+        <div className='nav_bar'>
+          <img src='' alt='' />
+          <div id='btn'>
+          <button>Login</button>
+          <button>Sign Up</button>
+        </div>
+        </div>  
+        
+
+        
+        <p className='description'>We do care about your mental health</p>
+        <div className='body'>
+            <div className='left-text'>
+             <h>Good news</h><br/>
+             <p>Positive mental health allows people to: <br/> Realize ntheir full potential. <br/> 
+             Cope with stresses of life.<br/> Work productively. <br/> Make meaningfull contribution to their communities.
+             </p>
+            </div>
+            <div className='right-text'>
+              <h>Bad news</h><br/>
+              <p>Unhappiness and decreased enjoyment of life.<br/> Family conflicts.<br/> Relationship difficulties.<br/>
+              Social isolation.<br/> Problems with tobacco and alcohol and other drugs.<br/>
+              Legal and financial problems.
+              </p>
+            </div> 
+        </div>
+    </div>
+  )
+}
