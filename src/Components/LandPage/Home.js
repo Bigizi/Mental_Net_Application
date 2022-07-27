b23e20cc6e1f459e935ff0f629b72504bc6fd7b0
Crispin <bigizinduwayo@gmail.com>
2022-07-27 09:57:11 +0200

    Landing page updated

--- src/Components/LandPage/Home.js
+++ src/Components/LandPage/Home.js
@@ -12,7 +12,6 @@ export default function Home() {
   return (
   
     <div className='container'>
-    {/* <img src='main' alt='' className='img' /> */}
       <Navbar onLoginClick={_=>setShowLoginForm(true)} />
         
         {
@@ -23,22 +22,20 @@ export default function Home() {
         <p className='description'>We do care about your mental health</p>
         <div className='body'>
             <div className='left-text'>
-             <h>Good news</h><br/>
+             <h>Good news:</h><br/><br/>
              <p>Positive mental health allows people to: <br/> Realize ntheir full potential. <br/> 
              Cope with stresses of life.<br/> Work productively. <br/> Make meaningfull contribution to their communities.
              </p>
             </div>
          
             <div className='right-text'>
-              <h>Bad news</h><br/>
+              <h>Bad news:</h><br/><br/>
               <p>Unhappiness and decreased enjoyment of life.<br/> Family conflicts.<br/> Relationship difficulties.<br/>
               Social isolation.<br/> Problems with tobacco and alcohol and other drugs.<br/>
               Legal and financial problems.
               </p>
             </div> 
-        </div>
-        {/* <SignUp /> */}
-        
+      </div> 
     </div>
   )
 }
